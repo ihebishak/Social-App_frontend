4cb0a4f131c46b957a6991650aa7bacb8eecab91
iheb_ishak <ihebishak8@gmail.com>
2021-11-08 20:49:04 +0100

    d

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,7 @@
+import { UserService } from './../../services/user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService,private snackBar: MatSnackBar) {
+   }
 
-  ngOnInit(): void {
+   public user = {
+    username:'',
+    password:'',
+    firstname:'',
+    lastname:'',
+    phone:'',
+    email:'',
+    imagepro:'',
+    imagecov:'',
+    bio:'',
+    }
+
+    ngOnInit(): void {}
+
+    formSubmit() {
+    console.log(this.user);
+    if(this.user.username==null || this.user.username=="")
+    { this.snackBar.open('Username is required', 'Close'); }
+    else
+    {
+      //addUser: userservice
+     this.userService.addUser(this.user).subscribe(
+      (data:any)=>{
+        //success
+        console.log(data)
+        if(data)
+        Swal.fire('Registered','You are Registered Successfully!','success');
+        else
+        Swal.fire('Username already exist!','set other Username which is unique', 'error');
+        },
+       (error)=>{
+        console.log(error)
+        this.snackBar.open('Something went wrong !! Try again.','close');
+      }
+     );
+  }
   }
 
 }
